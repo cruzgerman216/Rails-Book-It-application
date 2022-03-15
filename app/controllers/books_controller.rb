0864132b6d41a476d3d2685e194902d4fb309069
German Cruz <43277807+cruzgerman216@users.noreply.github.com>
2022-03-14 23:23:13 -0500

    Part 2 CRUD Operations

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,4 +11,41 @@ class BooksController < ApplicationController
     def show 
         @book = Book.find(params[:id])
     end
+
+    def new 
+        @book = Book.new
+    end
+
+    def create 
+        @book = Book.new(params.require(:book).permit(:title, :description))
+
+        if @book.save  
+            flash[:notice] = "Book was saved!"
+            redirect_to books_path
+        else 
+            render :new
+        end
+    end
+
+    def edit 
+        @book = Book.find(params[:id])
+    end
+
+    def update 
+        @book = Book.find(params[:id])
+
+        if @book.update(params.require(:book).permit(:title, :description))
+            flash[:message] = "Updated successfully"
+            redirect_to books_path
+        else 
+            render :edit
+        end
+    end
+
+    def destroy 
+        @book = Book.find(params[:id])
+        @book.destroy 
+        redirect_to books_path
+    end 
+
 end
