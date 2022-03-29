72cb89ab77536371392dfb50f938aa6e3562f1b5
German Cruz <43277807+cruzgerman216@users.noreply.github.com>
2022-03-28 19:39:40 -0500

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,13 @@ images = [
   "https://images-na.ssl-images-amazon.com/images/I/51uDbir0FNL._SX318_BO1,204,203,200_.jpg",
   "https://images-na.ssl-images-amazon.com/images/I/81NuITyKhiL.jpg",
 ]
+
+users = [
+  User.create(username: "johndoe123", email: "johndoe123@google.com", password: "password"),
+  User.create(username: "jimmyjohns456", email: "jimmyjohns456@google.com", password: "password")
+  User.create(username: "adminperson24", email: "admin@admin.com", password: "password")
+
+]
 (1..50).each do
-  Book.create(title: Faker::Book.title, description: Faker::Lorem.paragraph(sentence_count: 2), image_path: images[Random.rand(images.length)])
+  User.all[users.count].Book.create(title: Faker::Book.title, description: Faker::Lorem.paragraph(sentence_count: 2), image_path: images[Random.rand(images.length)])
 end
