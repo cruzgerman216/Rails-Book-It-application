fb6105a10572a280737598272d22fc0f5474b5ea
German Cruz <43277807+cruzgerman216@users.noreply.github.com>
2022-03-28 19:35:41 -0500

    Update pages_helper.rb

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,5 +1,14 @@
 module PagesHelper
-    def eight_random_books 
-        return 8.times.map { Book.all[Random.rand(Book.count)] } 
+  def eight_random_books
+    count = Book.all.count
+    books = []
+    return books if count == 0
+
+    if count < 8
+      books = count.times.map { Book.all[Random.rand(count)] }
+    else
+      books = 8.times.map { Book.all[Random.rand(Book.count)] }
     end
+    return books
+  end
 end
